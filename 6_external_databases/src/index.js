721f473ee100f85e6ba1e5a4f0dd484226ca9777
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-07-31 10:54:08 -0600

    Last minute commit

--- 6_external_databases/src/index.js
+++ 6_external_databases/src/index.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // fetch('http://whiskyhunter.net/api/distillery_data/')
+  // .then(r =>r.json())
+  // .then(data => console.log(data))
   //Fetch from https://dog.ceo/dog-api/
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(r => r.json())
+  .then(data => console.log(data))
   //Fetch from weather api
+  const lat = 12
+  const long = 30
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=${api_key}`)
+  .then(r => r.json())
+  .then(weather => console.log(weather))
 })

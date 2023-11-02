3fb7b7a32b5ca39bee58a7e25bb02bb562017256
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-11-02 11:50:19 -0600

    Lecture 4

--- 4_Communicating_with_the_server/src/index.js
+++ 4_Communicating_with_the_server/src/index.js
@@ -6,6 +6,50 @@ document.addEventListener('DOMContentLoaded', () => {
   // .then((r)=>r.json())
   // .then((data)=> )
   // .catch(e => console.log(e))
+  fetch("http://localhost:3000/stores/3")
+  .then(r =>  r.json())
+  .then(data => {
+    console.log(data)
+    renderHeader(data)
+  })
+
+  fetch("http://localhost:3000/books")
+  .then(r => r.json())
+  .then(books => {
+    console.log(books)
+    books.forEach((book)=>{
+      createBookCard(book)
+    })
+  })
+
+  
+  
+  const form = document.querySelector("#book-form")
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const new_obj = {
+      title: e.target['form-title'].value,
+      author: e.target.author.value,
+      price: parseFloat(document.querySelector("#form-price").value),
+      imageUrl: e.target.imageUrl.value,
+      inventory: parseInt(e.target.inventory.value)
+    }
+    fetch("http://localhost:3000/books", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(new_obj)
+    })
+    .then(r=>r.json())
+    .then(data=>console.log(data))
+    console.log(new_obj)
+    //UPDATE FRONTEND
+    createBookCard(new_obj)
+    //UPDATE BACKEND
+  })
+
+  console.log("We're running this line")
 
   // We can fetch either all the data or just one!
 
@@ -14,9 +58,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Yesterdays:
   // Renders Header
-  function renderHeader(){
+  function renderHeader(bookStore){
     document.querySelector('h1').textContent = bookStore.name;
   }
+
   // Renders Footer
   function renderFooter(){
     const footerDivs = document.querySelectorAll('footer div');
@@ -24,6 +69,41 @@ document.addEventListener('DOMContentLoaded', () => {
     footerDivs[1].textContent = bookStore.address;
     footerDivs[2].textContent = bookStore.hours;
   }
+
+  function createBookCard(book){
+    const li = document.createElement('li')
+    const title = document.createElement("h3")
+    const author = document.createElement("p")
+    const price = document.createElement("p")
+    const image = document.createElement("img")
+    const del = document.createElement("button")
+    del.textContent = "Delete"
+    title.textContent = book.title
+    author.textContent = book.author
+    price.textContent = book.price
+    image.src = book.imageUrl
+
+
+    li.append(title,author,price,image, del)
+    document.querySelector("#book-list").append(li)
+
+    price.addEventListener('click',()=>{
+        price.textContent = parseInt(price.textContent)+1
+    })
+
+    del.addEventListener('click',()=>{
+        li.remove()
+        // fetch delete
+    })
+    li.addEventListener('click',()=>{
+        if(image.src === book.imageUrl){
+        image.src = ""
+        }
+        else{
+        image.src = book.imageUrl
+        }
+    })
+    }
 
   // Lets take the logic of yesterdays card data rendering as well!
 
--- 4_Communicating_with_the_server/src/review.js
+++ 4_Communicating_with_the_server/src/review.js
@@ -1,3 +1,92 @@
-// Array manipulation
+// Object manipulation
+const obj = {
+    key: "value"
+}
+const variable = "key"
+console.log(obj["key"])
+console.log(obj[variable])
+obj["new_key"] = "new value"
+console.log(obj)
+function return_obj(a,b){
+    const new_obj = {
+        a: a,
+        b: b
+    }
+    return new_obj
+}
+console.log(return_obj(1,2))
 
-// Query Selectors
+for(let key in obj){
+    console.log(key + ":" + obj[key])
+}
+
+// event listeners
+const h1 = document.querySelector("h1")
+console.log(h1)
+h1.addEventListener('click',(e)=>{
+    console.log(e)
+    if (h1.textContent === "Something"){
+        h1.textContent = "Something else"
+    }
+    else{
+        h1.textContent = "Something"
+    }
+})
+
+function createBookCard(book){
+    const li = document.createElement('li')
+    const title = document.createElement("h3")
+    const author = document.createElement("p")
+    const price = document.createElement("p")
+    const image = document.createElement("img")
+    const del = document.createElement("button")
+    del.textContent = "Delete"
+    title.textContent = book.title
+    author.textContent = book.author
+    price.textContent = book.price
+    image.src = book.imageUrl
+
+
+    li.append(title,author,price,image, del)
+    document.querySelector("#book-list").append(li)
+
+    price.addEventListener('click',()=>{
+        price.textContent = parseInt(price.textContent)+1
+    })
+
+    del.addEventListener('click',()=>{
+        li.remove()
+    })
+    // li.addEventListener('mouseover',()=>{
+    //   image.src = book.imageUrl
+    // })
+    // li.addEventListener('mouseout',()=>{
+    //   image.src = ""
+    // })
+    li.addEventListener('click',()=>{
+        if(image.src === book.imageUrl){
+        image.src = ""
+        }
+        else{
+        image.src = book.imageUrl
+        }
+    })
+}
+ 
+
+const form = document.querySelector("#book-form")
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    console.log(e.target['form-title'].value)
+    console.log(e.target.title.value)
+    console.log(document.querySelector("#form-title").value)
+    const new_obj = {
+        title: e.target['form-title'].value,
+        author: e.target.author.value,
+        price: parseFloat(document.querySelector("#form-price").value),
+        imageUrl: e.target.imageUrl.value,
+        inventory: parseInt(e.target.inventory.value)
+    }
+    console.log(new_obj)
+    createBookCard(new_obj)
+})

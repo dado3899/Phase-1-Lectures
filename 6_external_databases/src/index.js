38f3c51e887b0371e2a455c522bd401e123ee590
David Doan <daviddoan@Jaycees-MacBook-Pro.local>
2023-12-22 14:00:37 -0700

    solution

--- 6_external_databases/src/index.js
+++ 6_external_databases/src/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   //Fetch from https://dog.ceo/dog-api/
   //Fetch from weather api
+  fetch('https://pokeapi.co/api/v2/pokemon/blastoise')
+  .then(r=>r.json())
+  .then(data=> console.log(data))
 })

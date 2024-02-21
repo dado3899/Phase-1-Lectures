14e74a110f94468f61e5b558f62d3c96809ce225
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-02-21 10:57:43 -0700

    Array solution

--- 2_DOM_Manipulation/src/index.js
+++ 2_DOM_Manipulation/src/index.js
@@ -2,91 +2,77 @@
 // How would we import this?
 // Well we would add 
 // <script src="src/newfile.js"></script>
-const bookStore = {
-  location: "Seattle",
-  address:'333 st ne Seattle wa 99999',
-  number: 9999999999,
-  name: 'Coding Books',
-  hours: 'Monday - Friday 9am - 6pm',
-  inventory: [
-      {
-          id:1,
-          title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-          author: 'Marjin Haverbeke',
-          price: 10.00,
-          reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
-          inventory: 10,
-          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-          
-      },
-      {
-          id:2,
-          title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-          author: 'Jon Duckett',
-          price: 45.75,
-          reviews: [{userID: 15, content:'good way to learn JQuery'}],
-          inventory: 2,
-          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-      },
-      {
-          id:3,
-          title: 'JavaScript: The Good Parts',
-          author: 'Douglas Crockford',
-          price: 36.00,
-          reviews: [{userID: 25, content:'I disagree with everything in this book'}, {userID: 250, content:'Only JS book anyone needs'}],
-          inventory: 8,
-          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-      },
-      {
-          id:4,
-          title: 'JavaScript: The Definitive Guide',
-          author: 'David Flanagan',
-          price: 25.50,
-          reviews: [{userID: 44, content:'Great intro to js book'}, {userID: 350, content:'It really is the Definitive guide'}],
-          inventory: 0,
-          imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-          
-      },
-      {
-          id:5,
-          title: 'You Don’t Know JS',
-          author: 'Kyle Simpson',
-          price: 6.00,
-          reviews: [{userID: 76, content:'You can find this for free online, no need to pay for it!'}],
-          inventory: 7,
-          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-      }, 
-      {
-          id:6,
-          title: 'Learn Enough JavaScript to Be Dangerous',
-          author: 'Michael Hartl',
-          price: 24.00,
-          reviews: [{userID: 50, content:'pretty good'}],
-          inventory: 5,
-          imageUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQyf6xSyTHc7a8mx17ikh6GeTohc88Hn0UgkN-RNF-h4iOwVlkW'
+console.log(bookStore)
 
-      },
-      {
-          id:7,
-          title: 'Cracking the Coding Interview',
-          author: 'Gayle Laakmann McDowell',
-          price: 49.95,
-          reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID:20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
-          inventory: 20,
-          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-
-      }
-  ]
-}
-
+// const str ="STRING"
+// str.toLowerCase()
 //array methods
+const arr = []
+const fruits = ["Apple","Orange"]
 //push adds to the end
-//unshift adds to the start
-//spread operator copies the array
-//shift removes the first element
+fruits.push("Watermelon")
+console.log(fruits)
 //pop removes the last element
-//slice changes the array to between two indices
-//splice returns the above but keeps the the original
+// const removedElement = fruits.pop()
+// console.log(removedElement)
+console.log(fruits)
+//unshift adds to the start
+fruits.unshift("Kiwi")
+console.log(fruits)
+//shift removes the first element
+// const shiftedElement = fruits.shift()
+// console.log(shiftedElement)
+console.log(fruits)
+//spread operator copies the array
+const newFruits = [...fruits]
+newFruits.push("Lemon")
+fruits.push("Coconut")
+console.log("Original: ", fruits)
+console.log("New: ",newFruits)
+//slice returns the array to between two indices, slice(startIndex,endIndex)
+const sliced = fruits.slice(0,2)
+console.log(sliced)
+console.log(fruits)
+//splice changes original array by removing indices, splice(startIndex,deletecount)
+//splice also lets you add to the middle!
+const spliced = fruits.splice(1,0,"ADDED","ADDED","ADDED","ADDED","ADDED","ADDED")
+console.log(spliced)
+console.log(fruits)
+//length returns the length
+const fruitLen= fruits.length
+console.log(fruitLen)
+// const obj = {
+//     a: 1
+// }
+// obj.a
+// obj["a"]
+
+//foreach
+fruits.forEach((fruit)=>{
+    if (fruit === "ADDED"){
+        console.log("We found the not fruit")
+    }
+    else{
+        console.log("This is our Fruit: ", fruit)
+    }
+})
+//map
+const mapVar = fruits.map((fruit)=>{
+    return 1
+    // if (fruit === "ADDED"){
+    //     return "Peach"
+    // }
+    // else{
+    //     return fruit
+    // }
+})
+// fruits.map(func)
+console.log(mapVar)
+function changeFruit(fruit){
+    return fruit+"s"
+}
+const callbackMap = fruits.map(changeFruit)
+console.log(callbackMap)
 
 // Lets Create a .querySelector()!
 // .getElementById
--- 2_DOM_Manipulation/src/review.js
+++ 2_DOM_Manipulation/src/review.js
@@ -1,9 +1,40 @@
 // Functions
+const r1 = funcExample(10)
+function funcExample(x){
+    console.log(x)
+    return x + " Hi"
+}
+// x = 10
+// x = dog
+const r2d2 = funcExample("Dog")
+console.log(r1,r2d2)
 // Regular
 // Anonomous
+const anon = function(){
+    console.log("In anon")
+}
+anon()
 // Arrow
+console.log(
+    () => {
+        console.log("Arrow")
+    })
 
 // Scope
 // Global
+const glob = "Global"
 // Function
+function funcScope(){
+    const func = "Function"
+    if(book.price<10 && book.inventory>0 ){
+        const ifVar= "If"
+        console.log(ifVar)
+        if(true){
+            const if2 = "If2"
+            console.log(if2)
+        }
+    }
+}
+funcScope()
+// console.log(func)
 // Block

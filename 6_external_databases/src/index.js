753fd46d24480d73e51a1173b7be9a0c2c3ff56f
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-02-26 11:35:12 -0700

    Lecture 6

--- 6_external_databases/src/index.js
+++ 6_external_databases/src/index.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   //Fetch from https://dog.ceo/dog-api/
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(r=>r.json())
+  .then(data=>{
+    console.log(data)
+    const list = document.querySelector('#book-list')
+    const img = document.createElement("img")
+    img.src = data.message
+    list.append(img)
+  })
   //Fetch from weather api
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=55.8617&lon=4.2583&appid=${apiKey}`)
+  .then(r=>r.json())
+  .then(data=>{
+    console.log(data)
+  })
 })
--- 6_external_databases/src/secretFile.js
+++
@@ -1,1 +0,0 @@
-const apiKey = "c82d5efadbe37e6a719e4c9825b64b6b"

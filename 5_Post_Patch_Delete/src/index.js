c8c8fa79d9be3c67d5c8859e5058a44817bb47c7
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-06-14 15:38:44 -0600

    Reverted lecture 5

--- 5_Post_Patch_Delete/src/index.js
+++ 5_Post_Patch_Delete/src/index.js
@@ -1,60 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Basic Get request (Read)
-  fetch('http://localhost:3000/books')
-  .then((r)=> r.json())
-  .then((books)=> {
-    books.forEach((book) => addBook(book))
-  })
 
-  fetch('http://localhost:3000/stores/2')
-  .then((r)=>r.json())
-  .then((store)=>{
-    renderHeader(store)
-    renderFooter(store)
-  })
-  
-  // const data2 = fetch('http://localhost:3000/users')
-  
-  // .then(r=>r.json())
-  // .then(data=>console.log(data))
-  // More in depth in async!
   // Post
-  function postBook(book){
-    fetch('http://localhost:3000/books',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(book)
-    })
-    .then((r)=>r.json())
-    .then((newBook)=>addBook(newBook))
-  }
+  
   // Delete
-  function deleteBook(book){
-    fetch(`http://localhost:3000/books/${book.id}`, {method: "DELETE"})
-  }
+
   // Patch
-  fetch('http://localhost:3000/books/2',{
-    method: 'PATCH',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body:JSON.stringify({
-      author: "Jon HG Duckett",
-      price: 1,
-      reviews: [
-        {
-          "userID": 15,
-          "content": "good way to learn JQuery"
-        },
-        {
-          "userID": 10,
-          "content": "good way to learn JQuery"
-        },
-      ],
-    })
-  })
 
   // Yesterdays:
   // Renders Header
@@ -68,47 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     footerDivs[1].textContent = store.address;
     footerDivs[2].textContent = store.hours;
   }
-  //Add a book
-  function addBook(book){
-    console.log(book)
-    const li = document.createElement("li")
-    const title = document.createElement("h3")
-    const author = document.createElement("p")
-    const price = document.createElement("p")
-    const img = document.createElement("img")
-    const button = document.createElement("button")
-    title.textContent = book.title
-    author.textContent = book.author
-    price.textContent = book.textContent
-    img.src = book.imageUrl
-    button.textContent = "Delete"
-    li.className = "List-Element"
-    li.append(title,author,price,img, button)
-    book.reviews.forEach((review)=> {
-      const newReview = document.createElement("p")
-      newReview.textContent = review.conten
-      li.append(newReview)
-    })
-    button.addEventListener('click',()=> {
-      li.remove()
-      deleteBook(book)
-    })
-    document.querySelector("#book-list").append(li)
-  }
-
-  // Book Form Event 
-  const form = document.querySelector("#book-form")
-  form.addEventListener('submit', (event)=> {
-    event.preventDefault()
-    const newBook = {
-      title: event.target.title.value,
-      author: event.target.author.value,
-      price: parseInt(event.target.price.value),
-      imageUrl: event.target.imageUrl.value,
-      inventory: parseInt(event.target.inventory.value),
-      reviews: []
-    }
-    console.log(newBook)
-    postBook(newBook)
-  })
+  //Add a book from yesterday
+  
 })
